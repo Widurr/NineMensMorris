e4a616f17db1f3d4add0d0781db0d0a1b4f78ad6
Monti90 <72450384+Monti90@users.noreply.github.com>
2021-05-15 18:00:02 +0200

    Added Sounds to project

---
+++ Assets/Scripts/Game/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+ 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "StartMenu")
+            Play("MenuTheme");
+        else
+            Play("PlayTheme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+
+            return;
+        }
+        s.source.Play();
+    }
+}
